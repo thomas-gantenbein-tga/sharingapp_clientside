14c383a41ff759fd27f9cb255c5b6624cfcaeb88
Thomas Gantenbein <tgantenbein@sunrise.ch>
2018-08-20 22:14:03 +0200

    #4: Dropdown für Kategorie hinzugefügt
    Closes #4.

--- app/src/main/java/shareapp/mobileapps/master/zhaw/ch/sharingapp_clientside/activities/CreateNewArticleActivity.java
+++ app/src/main/java/shareapp/mobileapps/master/zhaw/ch/sharingapp_clientside/activities/CreateNewArticleActivity.java
@@ -1,7 +1,12 @@
 package shareapp.mobileapps.master.zhaw.ch.sharingapp_clientside.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import shareapp.mobileapps.master.zhaw.ch.sharingapp_clientside.R;
 
@@ -11,5 +16,34 @@ public class CreateNewArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_article);
+
+        Spinner spinner = findViewById(R.id.spinnerArticleCategory);
+        String[] r = getResources().getStringArray(R.array.articleCategory);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, r) {
+            // nicht unbedingt nötig, aber hübsch: Erster Eintrag verschwindet wenn Dropdown aufgeht
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent)
+            {
+                View v = null;
+                // If this is the initial dummy entry, make it hidden
+                if (position == 0) {
+                    TextView tv = new TextView(getContext());
+                    tv.setHeight(0);
+                    tv.setVisibility(View.GONE);
+                    v = tv;
+                }
+                else {
+                    // Pass convertView as null to prevent reuse of special case views
+                    v = super.getDropDownView(position, null, parent);
+                }
+                // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                parent.setVerticalScrollBarEnabled(false);
+                return v;
+            }
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
