93cd733e43492f2e9fa285689735ca0aa5b487bf
Thomas Gantenbein <tgantenbein@sunrise.ch>
2018-08-26 21:28:34 +0200

    #15: Build wollte nicht mehr, weil der Button hier referenziert war

--- app/src/main/java/shareapp/mobileapps/master/zhaw/ch/sharingapp_clientside/activities/MainActivity.java
+++ app/src/main/java/shareapp/mobileapps/master/zhaw/ch/sharingapp_clientside/activities/MainActivity.java
@@ -2,8 +2,6 @@ package shareapp.mobileapps.master.zhaw.ch.sharingapp_clientside.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -28,15 +26,6 @@ public class MainActivity extends AppCompatActivity implements DataListener {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DataService dataService = new ServerDataService(this, Endpoint.LOCALHOST);
         //sobald die Daten hier sind, wird Methode receiveData ausgeführt
